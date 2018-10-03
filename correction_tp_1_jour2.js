f6d7eb2d98d7c65e9831fbee40dd5f9d7cd4c289
nsekecharles <nsekecharles@users.noreply.github.com>
2018-10-03 15:46:48 +0200

    Update correction_tp_1_jour2.js

--- correction_tp_1_jour2.js
+++ correction_tp_1_jour2.js
@@ -1,1 +1,55 @@
 
+// Question 1:
+var karine = {
+              premom: 'Karine', 
+              dateDeNaissance: new Date(1991, 4, 16), 
+              moyenne: 12
+             };
+var Celia = {
+              premom: 'Karine', 
+              dateDeNaissance: new Date(1995, 5, 9), 
+              moyenne: 12
+             };
+var Christophe =  {
+              premom: 'Christophe', 
+              dateDeNaissance: new Date(1997, 9, 9), 
+              moyenne: 12
+             };
+
+
+// Question 2 :
+
+var personnes = new Array(Karine, Celia, Christophe);
+
+
+// Question 3 : 
+
+for(var i = 0; i < personnes.length; i++) {
+  var today = new Date();
+  console.log(personnes[i].prenom + ' : ' + today.getFullYear() - personnes[i].dateDeNaissance.getFullYear());
+}
+
+
+// Question 4 : 
+
+for(var i = 0; i < personnes.length; i++) {
+  var today = new Date();
+  if(today.getFullYear() - personnes[i].dateDeNaissance.getFullYear() > 25) {
+      console.log(personnes[i].prenom);
+  }
+  
+  // Question 5 : 
+  for(var i = 0; i < personnes.length; i++) {
+    if(personnes[i].prenom.charAt(0) === 'C') {
+      console.log(personnes[i].prenom);
+    }
+  }
+
+
+// Question 6 :
+  for(var i = 0; i < personnes.length; i++) {
+    
+  }
+  
+
+
